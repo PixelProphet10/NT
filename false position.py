8e597b8d5b27d0d9c434231310017331fc463521
Samy109005 <samargamare555@gmail.com>
2025-02-19 22:43:11 +0530

    Update false position.py

--- false position.py
+++ false position.py
@@ -3,7 +3,6 @@ import random
 def function(x):
     return x**3
 
-
 while True:
     x1 = random.randint(-100, 100)
     if function(x1) > 0:
@@ -12,12 +11,14 @@ while True:
             print(x1, x2)
             break
 
-for i in range(1000):
+for i in range(10_000_000):
          #start false position here
-        x3 = (x1 + x2)/2
-        if (function(x3) > 0):
-            x1 = x3
-        elif function(x3) < 0:
-            x2 = x3
+         x3 = x1 - ((x2 - x1)*function(x1))/(function(x2) - function(x1))
+         if (function(x3) > 0):
+             x1 = x3
+         elif function(x3) < 0:
+             x2 = x3
+         else:
+              x3 = 0
+              break
 print(x1, x2, x3)
-
